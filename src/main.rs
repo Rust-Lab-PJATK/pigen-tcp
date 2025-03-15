ea36ba8c3bc92572cbea2b4407f650413e6e5f74
Maciej Pędzich <contact@maciejpedzi.ch>
2025-03-15 13:08:53 +0100

    Revert to streaming one digit at a time

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ fn main() -> Result<(), Box<dyn Error>> {
             // https://homepages.cwi.nl/~steven/abc/programmers/examples.html
             // (Section: Numbers, Subsection: Pi)
 
-            let mut batch = String::with_capacity(100);
             let mut k = Integer::from(2);
             let mut a = Integer::from(4);
             let mut b = Integer::from(1);
@@ -43,14 +42,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                 let mut d1 = Integer::from(&a1 / &b1);
 
                 while d == d1 {
-                    batch.push_str(&d.to_string());
-                    
-                    if batch.len() == batch.capacity() {
-                        if let Err(_e) = stream.write_all(batch.as_ref()) {
-                            break 'pi;
-                        }
-                        
-                        batch.clear();
+                    if let Err(_e) = stream.write_all(d.to_string().as_ref()) {
+                        break 'pi;
                     }
 
                     a %= &b;
